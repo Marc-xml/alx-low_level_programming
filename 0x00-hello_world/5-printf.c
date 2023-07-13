b3d34b7a5b60b4e84ce7fd30f1a0e976f47bf3a8
Your Name <marcronaldonganyi@gmail.com>
2023-07-13 14:13:41 -0700

    Task 7

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: prints a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
