777e3fc0e98e2581ae5cdafb9d28099651b95802
Your Name <marcronaldonganyi@gmail.com>
2023-07-19 09:46:07 -0700

    task 6 reloaded

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - function to compute absolute val
+ * @n: input for function
+ * Return: Rsults to n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
