9fcf3fef936b6e2e0619ae124c110dbeaa8e78ed
Your Name <marcronaldonganyi@gmail.com>
2023-08-04 01:56:54 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - prints name of program
+ * @argc: number of parametsr
+ * @argv: parameters being held
+ * Return:Always 0 success
+ */
+int main(int __attribute((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
