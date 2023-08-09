af1196dd1ff147099068b582f09fc07be9043bfe
Your Name <marcronaldonganyi@gmail.com>
2023-08-09 08:07:41 -0700

    task 0.1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	a = malloc(size * sizeof(char));
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < a; i++)
+	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
 	}
