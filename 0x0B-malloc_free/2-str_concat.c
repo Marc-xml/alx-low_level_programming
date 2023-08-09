7bc07960cbe471f606f5216c397f56c4d4cd446c
Your Name <marcronaldonganyi@gmail.com>
2023-08-09 09:10:00 -0700

    task 2.2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,12 +12,12 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-		s1 = '';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '';
-	while (s1 != '\0')
+		s2 = "";
+	while (s1[a] != '\0')
 		a++;
-	while (s2 != '\0')
+	while (s2[b] != '\0')
 		b++;
 	s = malloc((a + 1) * sizeof(char)) + malloc((b + 1) * sizeof(char));
 	for (i = 0; s1[i] != '\0'; i++)
