6dec45dd922f89ca051b02e891bf4a05c448b2d0
Your Name <marcronaldonganyi@gmail.com>
2023-08-14 22:52:57 -0700

    task dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,9 +5,10 @@
  * struct dog - dog structure
  *
  * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
@@ -16,7 +17,13 @@ struct dog
 };
 
 /**
- * dog_m - Typedef for struct dog
- */
-typedef struct dog dog_m;
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* struct dog */
